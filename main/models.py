1d1f2e5f78d87110587b335282a0026991ac6d23
Skhmelyuk <skhmelyuk1985@gmail.com>
2026-05-17 14:16:27 +0300

    lesson-03

--- main/models.py
+++ main/models.py
@@ -1,12 +1,42 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.urls import reverse
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, db_index=True, verbose_name="Ім'я категорії")
+    slug = models.SlugField(max_length=50, unique=True, verbose_name="Слаг для url")
+
+
+    class Meta:
+        verbose_name="Категорія"
+        verbose_name_plural="Категорії"
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def get_absolute_url(selt):
+        return reverse("main:post_list_by_category", args=[selt.slug])
+
+
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    image = models.ImageField(upload_to='post_images/%Y/%m/%d', blank=True)
+    content = models.TextField(blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    views = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = "Пост"
+        verbose_name_plural = "Пости"
 
     def __str__(self):
-        return f'{self.title} by {self.author.username}'
+        return f"{self.title} - {self.created_at}"
+    
+    def get_absolute_url(self):
+        return reverse("main:post_detail", args=[self.id, self.slug])
