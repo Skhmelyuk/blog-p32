433e2ba0d5831f5623229185a29ddc426c6106b0
Skhmelyuk <skhmelyuk1985@gmail.com>
2026-05-17 07:11:10 +0300

    lesson-02

--- main/views.py
+++ main/views.py
@@ -1,27 +1,11 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from .models import Post
 
 
 def post_list(request):
 
-    posts = [
-        {
-            "id": "1",
-            "title": "Django install",
-            "content": "With Django, you can take web applications from concept to launch in a matter of hours. Django takes care of much of the hassle of web development, so you can focus on writing your app without needing to reinvent the wheel. It’s free and open source.",
-        },
-        {
-            "id": "2",
-            "title": "Python install",
-            "content": "With Django, you can take web applications from concept to launch in a matter of hours. Django takes care of much of the hassle of web development, so you can focus on writing your app without needing to reinvent the wheel. It’s free and open source.",
-        },
-        {
-            "id": "3",
-            "title": "HTML shablon",
-            "content": "With Django, you can take web applications from concept to launch in a matter of hours. Django takes care of much of the hassle of web development, so you can focus on writing your app without needing to reinvent the wheel. It’s free and open source.",
-        }
-    ]
-
+    posts = Post.objects.all().order_by('-created_at')
     context = {"title": "Home page", "posts": posts}
 
-    # return render(request, "main/posts_list.html", context)
-    return HttpResponse("Привіт юзер! Чим я можу вам допомогти?")
+    return render(request, "main/posts_list.html", context)
+    
