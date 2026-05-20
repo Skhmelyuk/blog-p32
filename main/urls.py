123686d559eeddfbb1bcf95310d7b150ec5d0ecc
Skhmelyuk <skhmelyuk1985@gmail.com>
2026-05-20 22:58:02 +0300

    lesson-04

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,8 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.post_list, name="post_list"),
+    path('posts/<slug:category_slug>/', views.post_list, name="post_list_by_category"),
+    path('post/<int:id>/<slug:slug>/', views.post_detail, name="post_detail"),
+ 
+
 ]
